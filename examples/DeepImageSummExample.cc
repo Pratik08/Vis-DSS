a7d352d7f0388feaaa78ab94f205879dc2903121
Pratik Dubal <pratik.dubal08@gmail.com>
2018-08-22 15:56:30 +0530

    chore(examples): Linting all examples.

--- examples/DeepImageSummExample.cc
+++ examples/DeepImageSummExample.cc
@@ -1,14 +1,12 @@
-#include "DeepSimImageSummarizer.h"
-#include "DeepCoverImageSummarizer.h"
-#include <iostream>
-#include "opencv2/opencv.hpp"
+#include <math.h>
+#include <dirent.h>
+#include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include "DeepSimImageSummarizer.h"
+#include "DeepCoverImageSummarizer.h"
 #include "arguments.h"
-#include "math.h"
-#include<dirent.h>
-using namespace std;
-using namespace cv;
 
 char* directory;
 char* videoSaveFile;
@@ -37,19 +35,19 @@ char* featureLayer;
 int summary_grid = 60;
 char* help;
 
-Arg Arg::Args[]={
-    Arg("directory", Arg::Req, directory, "Input Image Collection Directory",Arg::SINGLE),
-    Arg("imageSaveFile", Arg::Req, imageSaveFile, "Output Summary Image File",Arg::SINGLE),
-    Arg("summaryModelSim", Arg::Opt, summaryFunctionSim, "Summarization Model -- 0: DisparityMin, 1: MMR, 2: FacilityLocation, 3: GraphCut, 4: SaturatedCoverage",Arg::SINGLE),
-    Arg("summaryModelCover", Arg::Opt, summaryFunctionCover, "Summarization Model -- 0: FeatureBasedFunction, 1: Set Cover, 2: Probabilistic Set Cover",Arg::SINGLE),
-    Arg("simcover", Arg::Req, simcover, "0: Similarity Based Functions, 1: Coverage Based Functions",Arg::SINGLE),
-    Arg("summaryAlgo", Arg::Req, summaryAlgo, "Summarization Algorithm: 0: Budgeted Summarization, 1: Stream Summarization, 2: Coverage Summarization",Arg::SINGLE),
-    Arg("summarygrid", Arg::Opt, summary_grid, "Size of a image in the full grid",Arg::SINGLE),
-    Arg("featureLayer", Arg::Req, featureLayer, "Layer Name for Feature Extraction",Arg::SINGLE),
-    Arg("network_file", Arg::Req, network_file, "Input Network File",Arg::SINGLE),
-    Arg("trained_file", Arg::Req, trained_file, "Trained Model File",Arg::SINGLE),
-    Arg("mean_file", Arg::Req, mean_file, "Mean File",Arg::SINGLE),
-    Arg("label_file", Arg::Req, label_file, "Label File",Arg::SINGLE),
+Arg Arg::Args[] = {
+    Arg("directory", Arg::Req, directory, "Input Image Collection Directory", Arg::SINGLE),
+    Arg("imageSaveFile", Arg::Req, imageSaveFile, "Output Summary Image File", Arg::SINGLE),
+    Arg("summaryModelSim", Arg::Opt, summaryFunctionSim, "Summarization Model -- 0: DisparityMin, 1: MMR, 2: FacilityLocation, 3: GraphCut, 4: SaturatedCoverage", Arg::SINGLE),
+    Arg("summaryModelCover", Arg::Opt, summaryFunctionCover, "Summarization Model -- 0: FeatureBasedFunction, 1: Set Cover, 2: Probabilistic Set Cover", Arg::SINGLE),
+    Arg("simcover", Arg::Req, simcover, "0: Similarity Based Functions, 1: Coverage Based Functions", Arg::SINGLE),
+    Arg("summaryAlgo", Arg::Req, summaryAlgo, "Summarization Algorithm: 0: Budgeted Summarization, 1: Stream Summarization, 2: Coverage Summarization", Arg::SINGLE),
+    Arg("summarygrid", Arg::Opt, summary_grid, "Size of a image in the full grid", Arg::SINGLE),
+    Arg("featureLayer", Arg::Req, featureLayer, "Layer Name for Feature Extraction", Arg::SINGLE),
+    Arg("network_file", Arg::Req, network_file, "Input Network File", Arg::SINGLE),
+    Arg("trained_file", Arg::Req, trained_file, "Trained Model File", Arg::SINGLE),
+    Arg("mean_file", Arg::Req, mean_file, "Mean File", Arg::SINGLE),
+    Arg("label_file", Arg::Req, label_file, "Label File", Arg::SINGLE),
     Arg("budget", Arg::Opt, budget, "Budget for summarization (if summarization algo is 0)", Arg::SINGLE),
     Arg("thredshold", Arg::Opt, thresh, "Threshold for summarization (if summarization algo is 1)", Arg::SINGLE),
     Arg("coverage fraction", Arg::Opt, coverfrac, "coverage fraction for summarization (if summarization algo is 2)", Arg::SINGLE),
@@ -58,67 +56,53 @@ Arg Arg::Args[]={
 };
 
 int main(int argc, char** argv) {
-
-  bool parse_was_ok = Arg::parse(argc,(char**)argv);
-  if (!parse_was_ok) {
-      Arg::usage(); exit(-1);
-  }
-  DIR *dir;
-  std::string dirName = std::string(directory);
-  std::string imgName;
-  struct dirent *ent;
-  CaffeClassifier cc(network_file, trained_file, mean_file, label_file);
-  std::vector<cv::Mat> ImageCollection = std::vector<cv::Mat>();
-  std::cout << dirName << "\n";
-  dir = opendir (directory);
-  if (dir != NULL) {
-      while ((ent = readdir (dir)) != NULL) {
-          imgName = dirName + "/" + ent->d_name;
-          std::cout<<imgName<<"\n";
-          cv::Mat img = cv::imread(imgName);
-          if(!img.empty())
-              ImageCollection.push_back(img);
+    bool parse_was_ok = Arg::parse(argc, (char**)argv);
+    if (!parse_was_ok) {
+        Arg::usage(); exit(-1);
+    }
+    DIR *dir;
+    std::string dirName = std::string(directory);
+    std::string imgName;
+    struct dirent *ent;
+    CaffeClassifier cc(network_file, trained_file, mean_file, label_file);
+    std::vector<cv::Mat> ImageCollection = std::vector<cv::Mat>();
+    std::cout << dirName << "\n";
+    dir = opendir(directory);
+    if (dir != NULL) {
+        while ((ent = readdir(dir)) != NULL) {
+            imgName = dirName + "/" + ent->d_name;
+            std::cout << imgName << "\n";
+            cv::Mat img = cv::imread(imgName);
+            if (!img.empty())
+                ImageCollection.push_back(img);
         }
-  }
-  if (simcover == 0)
-  {
-      DeepSimImageSummarizer IS(ImageCollection, cc, featureLayer, summaryFunctionSim);
-      IS.extractFeatures();
-      IS.computeKernel();
-      if (summaryAlgo == 0)
-      {
-          IS.summarizeBudget(budget);
-      }
-      else if (summaryAlgo == 1)
-      {
-          IS.summarizeStream(thresh);
-      }
-      else if (summaryAlgo == 2)
-      {
-          IS.summarizeCover(coverfrac);
-      }
-      IS.displayAndSaveSummaryMontage(imageSaveFile, summary_grid);
     }
-    else
-    {
-      DeepCoverImageSummarizer IS(ImageCollection, cc, featureLayer, summaryFunctionCover,
-        FeatureBasedFnType, true);
-      IS.extractFeatures();
-      if (summaryAlgo == 0)
-      {
-          IS.summarizeBudget(budget);
-      }
-      else if (summaryAlgo == 1)
-      {
-          std::cout << "Streaming algorithms not implemented for coverage functions\n";
-      }
-      else if (summaryAlgo == 2)
-      {
-          IS.summarizeCover(coverfrac);
-      }
-      IS.displayAndSaveSummaryMontage(imageSaveFile, summary_grid);
+    if (simcover == 0) {
+        DeepSimImageSummarizer IS(ImageCollection, cc, featureLayer, summaryFunctionSim);
+        IS.extractFeatures();
+        IS.computeKernel();
+        if (summaryAlgo == 0) {
+            IS.summarizeBudget(budget);
+        } else if (summaryAlgo == 1) {
+            IS.summarizeStream(thresh);
+        } else if (summaryAlgo == 2) {
+            IS.summarizeCover(coverfrac);
+        }
+        IS.displayAndSaveSummaryMontage(imageSaveFile, summary_grid);
+    } else {
+        DeepCoverImageSummarizer IS(ImageCollection, cc, featureLayer, summaryFunctionCover,
+                                    FeatureBasedFnType, true);
+        IS.extractFeatures();
+        if (summaryAlgo == 0) {
+            IS.summarizeBudget(budget);
+        } else if (summaryAlgo == 1) {
+            std::cout << "Streaming algorithms not implemented for coverage functions\n";
+        } else if (summaryAlgo == 2) {
+            IS.summarizeCover(coverfrac);
+        }
+        IS.displayAndSaveSummaryMontage(imageSaveFile, summary_grid);
     }
-   // When everything done, release the video capture object
-   // Closes all the frames
-  return 0;
+    // When everything done, release the video capture object
+    // Closes all the frames
+    return 0;
 }
--- examples/DeepVideoSummExample.cc
+++ examples/DeepVideoSummExample.cc
@@ -1,13 +1,10 @@
-#include "DeepSimVideoSummarizer.h"
-#include "DeepCoverVideoSummarizer.h"
-#include <iostream>
-#include "opencv2/opencv.hpp"
+#include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include "DeepSimVideoSummarizer.h"
+#include "DeepCoverVideoSummarizer.h"
 #include "arguments.h"
-
-using namespace std;
-using namespace cv;
 
 char* videoFile;
 char* videoSaveFile;
@@ -33,20 +30,20 @@ char* label_file;
 char* featureLayer;
 char* help;
 
-Arg Arg::Args[]={
-    Arg("videoFile", Arg::Req, videoFile, "Input Video File",Arg::SINGLE),
-    Arg("videoSaveFile", Arg::Req, videoSaveFile, "Input Video File",Arg::SINGLE),
-    Arg("summaryModelSim", Arg::Opt, summaryFunctionSim, "Summarization Model -- 0: DisparityMin, 1: MMR, 2: FacilityLocation, 3: GraphCut, 4: SaturatedCoverage",Arg::SINGLE),
-    Arg("summaryModelCover", Arg::Opt, summaryFunctionCover, "Summarization Model -- 0: FeatureBasedFunction, 1: Set Cover, 2: Probabilistic Set Cover",Arg::SINGLE),
-    Arg("simcover", Arg::Req, simcover, "0: Similarity Based Functions, 1: Coverage Based Functions",Arg::SINGLE),
-    Arg("segmentType", Arg::Req, segmentType, "Segment Type -- 0: Fixed Length Segments, 1: Segments based on Shot Detectors",Arg::SINGLE),
-    Arg("summaryAlgo", Arg::Req, summaryAlgo, "Summarization Algorithm: 0: Budgeted Summarization, 1: Stream Summarization, 2: Coverage Summarization",Arg::SINGLE),
-    Arg("featureLayer", Arg::Req, featureLayer, "Layer Name for Feature Extraction",Arg::SINGLE),
-    Arg("network_file", Arg::Req, network_file, "Input Network File",Arg::SINGLE),
-    Arg("trained_file", Arg::Req, trained_file, "Trained Model File",Arg::SINGLE),
-    Arg("mean_file", Arg::Req, mean_file, "Mean File",Arg::SINGLE),
-    Arg("label_file", Arg::Req, label_file, "Label File",Arg::SINGLE),
-	  Arg("snippetLength", Arg::Opt, snippetLength, "Snippet Length", Arg::SINGLE),
+Arg Arg::Args[] = {
+    Arg("videoFile", Arg::Req, videoFile, "Input Video File", Arg::SINGLE),
+    Arg("videoSaveFile", Arg::Req, videoSaveFile, "Input Video File", Arg::SINGLE),
+    Arg("summaryModelSim", Arg::Opt, summaryFunctionSim, "Summarization Model -- 0: DisparityMin, 1: MMR, 2: FacilityLocation, 3: GraphCut, 4: SaturatedCoverage", Arg::SINGLE),
+    Arg("summaryModelCover", Arg::Opt, summaryFunctionCover, "Summarization Model -- 0: FeatureBasedFunction, 1: Set Cover, 2: Probabilistic Set Cover", Arg::SINGLE),
+    Arg("simcover", Arg::Req, simcover, "0: Similarity Based Functions, 1: Coverage Based Functions", Arg::SINGLE),
+    Arg("segmentType", Arg::Req, segmentType, "Segment Type -- 0: Fixed Length Segments, 1: Segments based on Shot Detectors", Arg::SINGLE),
+    Arg("summaryAlgo", Arg::Req, summaryAlgo, "Summarization Algorithm: 0: Budgeted Summarization, 1: Stream Summarization, 2: Coverage Summarization", Arg::SINGLE),
+    Arg("featureLayer", Arg::Req, featureLayer, "Layer Name for Feature Extraction", Arg::SINGLE),
+    Arg("network_file", Arg::Req, network_file, "Input Network File", Arg::SINGLE),
+    Arg("trained_file", Arg::Req, trained_file, "Trained Model File", Arg::SINGLE),
+    Arg("mean_file", Arg::Req, mean_file, "Mean File", Arg::SINGLE),
+    Arg("label_file", Arg::Req, label_file, "Label File", Arg::SINGLE),
+    Arg("snippetLength", Arg::Opt, snippetLength, "Snippet Length", Arg::SINGLE),
     Arg("budget", Arg::Opt, budget, "Budget for summarization (if summarization algo is 0)", Arg::SINGLE),
     Arg("thredshold", Arg::Opt, thresh, "Threshold for summarization (if summarization algo is 1)", Arg::SINGLE),
     Arg("coverage fraction", Arg::Opt, coverfrac, "coverage fraction for summarization (if summarization algo is 2)", Arg::SINGLE),
@@ -55,51 +52,37 @@ Arg Arg::Args[]={
 };
 
 int main(int argc, char** argv) {
-
-  bool parse_was_ok = Arg::parse(argc,(char**)argv);
-  if (!parse_was_ok) {
-      Arg::usage(); exit(-1);
-  }
-  CaffeClassifier cc(network_file, trained_file, mean_file, label_file);
-  if (simcover == 0)
-  {
-      DeepSimVideoSummarizer VS(videoFile, cc, featureLayer, summaryFunctionSim, segmentType, snippetLength);
-      VS.extractFeatures();
-      VS.computeKernel();
-      if (summaryAlgo == 0)
-      {
-          VS.summarizeBudget(budget);
-      }
-      else if (summaryAlgo == 1)
-      {
-          VS.summarizeStream(thresh);
-      }
-      else if (summaryAlgo == 2)
-      {
-          VS.summarizeCover(coverfrac);
-      }
-      VS.playAndSaveSummaryVideo(videoSaveFile);
+    bool parse_was_ok = Arg::parse(argc, (char**)argv);
+    if (!parse_was_ok) {
+        Arg::usage(); exit(-1);
     }
-    else
-    {
-      DeepCoverVideoSummarizer VS(videoFile, cc, featureLayer, summaryFunctionCover,
-        FeatureBasedFnType, segmentType, snippetLength, true);
-      VS.extractFeatures();
-      if (summaryAlgo == 0)
-      {
-          VS.summarizeBudget(budget);
-      }
-      else if (summaryAlgo == 1)
-      {
-          std::cout << "Streaming algorithms not implemented for coverage functions\n";
-      }
-      else if (summaryAlgo == 2)
-      {
-          VS.summarizeCover(coverfrac);
-      }
-      VS.playAndSaveSummaryVideo(videoSaveFile);
+    CaffeClassifier cc(network_file, trained_file, mean_file, label_file);
+    if (simcover == 0) {
+        DeepSimVideoSummarizer VS(videoFile, cc, featureLayer, summaryFunctionSim, segmentType, snippetLength);
+        VS.extractFeatures();
+        VS.computeKernel();
+        if (summaryAlgo == 0) {
+            VS.summarizeBudget(budget);
+        } else if (summaryAlgo == 1) {
+            VS.summarizeStream(thresh);
+        } else if (summaryAlgo == 2) {
+            VS.summarizeCover(coverfrac);
+        }
+        VS.playAndSaveSummaryVideo(videoSaveFile);
+    } else {
+        DeepCoverVideoSummarizer VS(videoFile, cc, featureLayer, summaryFunctionCover,
+                                    FeatureBasedFnType, segmentType, snippetLength, true);
+        VS.extractFeatures();
+        if (summaryAlgo == 0) {
+            VS.summarizeBudget(budget);
+        } else if (summaryAlgo == 1) {
+            std::cout << "Streaming algorithms not implemented for coverage functions\n";
+        } else if (summaryAlgo == 2) {
+            VS.summarizeCover(coverfrac);
+        }
+        VS.playAndSaveSummaryVideo(videoSaveFile);
     }
-   // When everything done, release the video capture object
-   // Closes all the frames
-  return 0;
+    // When everything done, release the video capture object
+    // Closes all the frames
+    return 0;
 }
